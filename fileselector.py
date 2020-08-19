b9729ec7465e2aa0a8c50feca6af50a9f404b28f
simrat singh <simrat.khokhar@gmail.com>
2020-08-20 01:08:52 +0530

    parsing file(s) in dictionary with absolute path and basename. Added button to move item up/down

--- fileselector.py
+++ fileselector.py
@@ -5,9 +5,10 @@ import os
 class FileSelector:
     def __init__(self):
         self.filelist = []
+        self.file_dictionary = {}
         self.index = -1
 
-    def selectFolder(self, event=None, path=None):
+    def selectFolder(self, path=None):
         self.clearList()
         if path is None:
             folder = tkfile.askdirectory()
@@ -24,29 +25,26 @@ class FileSelector:
                 self.filelist.append(pdfdoc)
         return True
 
-    def selectFolder2(self, event=None, path=None):
+    def selectFolder2(self, spath):
         self.clearList()
-        if path is None:
-            ifolder = tkfile.askopenfilenames(
-                    filetypes=(("PDF", "*.pdf"), ("All", "*.*"))
-                    )
-            #
-            # check for 'cancel' on dialog
-            #
-            if not folder:
-                return False
-        else:
-            folder = path
-        os.chdir(folder)
-        for pdfdoc in os.listdir(folder):
-            if pdfdoc.lower().endswith("pdf"):
-                self.filelist.append(pdfdoc)
+        filelist = tkfile.askopenfilenames(
+                title = "Select PDF file(s)",
+                initialdir = spath,
+                filetypes = (("PDF", "*.pdf"), ("All", "*.*"))
+                )
+        #
+        # check for 'cancel' on dialog
+        #
+        if not filelist:
+            return False
+        for f in filelist:
+            self.file_dictionary[os.path.basename(f)] = f
         return True
 
     def clearList(self):
-        self.filelist.clear()
+        self.file_dictionary.clear()
 
     def getList(self):
-        return self.filelist
+        return self.file_dictionary
 
 # ---- end ----
--- run_pdfmerger.py
+++ run_pdfmerger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import tkinter as tk
+import os
 
 from fileselector import FileSelector
 
@@ -17,16 +18,20 @@ class Application(tk.Frame):
         self._setLayout()
 
     def _createWidgets(self):
-        self.grid(row=6, column=5, sticky="nsew")
+        self.grid(row=7, column=5, sticky="nsew")
         self.master.minsize(300, 300)
-        self.browse = tk.Button(self, text="Browse")
-        self.process = tk.Button(self, text="Process")
-        self.lbox = tk.Listbox(self)
         self.label2 = tk.Label(self, text="File Name")
         self.entry = tk.Entry(self) 
+        self.browse = tk.Button(self, text="Browse")
+        self.process = tk.Button(self, text="Process")
+        self.lbox = tk.Listbox(self, selectmode=tk.SINGLE) #tk.EXTENDED)
+        self.moveup = tk.Button(self, text="Up")
+        self.movedown = tk.Button(self, text="Down")
 
     def _bindEvents(self):
         self.browse.bind('<ButtonRelease-1>', self._fillListBox)
+        self.moveup.bind('<ButtonRelease-1>', self._moveup)
+        self.movedown.bind('<ButtonRelease-1>', self._movedown)
 
     def _setLayout(self):
         self.label2.grid(row=0, column=0, 
@@ -35,22 +40,58 @@ class Application(tk.Frame):
         self.entry.grid(row=0, column=1, 
                         columnspan=2, 
                         padx=2, pady=2)
+        self.browse.grid(row=1, column=4)
+        self.process.grid(row=2, column=4)
         self.lbox.grid(row=1, column=0, 
                         rowspan=5, columnspan=4,
                         sticky='nsew', 
                         padx=2, pady=0)
-        self.browse.grid(row=1, column=4)
-        self.process.grid(row=2, column=4)
+        self.moveup.grid(row=6, column=2)
+        self.movedown.grid(row=6, column=3)
 
     def _fillListBox(self, event=None):
-        if self.fs.selectFolder():
+        if self.fs.selectFolder2(spath = os.environ['HOME']):
             #
             # folder parsing is success
             # need to populate list-box
             #
             self.lbox.delete(0, 'end')
-            for item in self.fs.getList():
+            for item in self.fs.getList().keys():
                 self.lbox.insert('end', item)
+
+    def _moveup(self, event=None):
+        index = self.lbox.curselection()
+        if not index:
+            return
+        #
+        # list of selected items returned
+        # in case of tk.EXTENDED
+        #
+        for pos in index:
+            if pos == 0:
+                continue
+            text = self.lbox.get(pos)
+            self.lbox.delete(pos)
+            self.lbox.insert(pos - 1, text)
+            #
+            # to keep the selection
+            #
+            self.lbox.selection_set(pos - 1)
+
+    def _movedown(self, event=None):
+        index = self.lbox.curselection()
+        if not index:
+            return
+        for pos in index:
+            if pos == self.lbox.size() - 1:
+                # 
+                # checking if current selection is last item
+                #
+                continue
+            text = self.lbox.get(pos)
+            self.lbox.delete(pos)
+            self.lbox.insert(pos + 1, text)
+            self.lbox.selection_set(pos + 1)
 
 
 if __name__ == "__main__":
